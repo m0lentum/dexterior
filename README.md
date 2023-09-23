bac7803dfcf19a2ff14e0fb46d9f2758bbf9f132
Mikael Myyrä <mikael.myyrae@gmail.com>
2023-09-23 23:37:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Dexterior is a Rust library for solving partial differential equations
+using Discrete Exterior Calculus (DEC).
+It's an early work in progress and will be documented at a later date.
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ (import inputs.rust-overlay) ];
+        };
+
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rust
+            pkgs.lld
+          ];
+        };
+      });
+}
