1d41de9abdc1be3df5be4fbfc65d6da32e97f8d4
Mikael Myyrä <mikael.myyrae@gmail.com>
2023-11-19 00:00:41 +0200

    examples: Add boundary condition and scalar multiplication to membrane.rs

--- examples/membrane.rs
+++ examples/membrane.rs
@@ -6,17 +6,17 @@
 
 use dexterior as dex;
 
-type Pressure = dex::Cochain<0, dex::Dual>;
-type Flux = dex::Cochain<1, dex::Primal>;
+type Pressure = dex::Cochain<0, dex::Primal>;
+type Velocity = dex::Cochain<1, dex::Primal>;
 
 struct State {
     p: Pressure,
-    q: Flux,
+    v: Velocity,
 }
 
 struct Ops {
-    p_step: Box<dyn dex::Operator<Input = Flux, Output = Pressure>>,
-    q_step: Box<dyn dex::Operator<Input = Pressure, Output = Flux>>,
+    p_step: Box<dyn dex::Operator<Input = Velocity, Output = Pressure>>,
+    v_step: Box<dyn dex::Operator<Input = Pressure, Output = Velocity>>,
 }
 
 fn main() {
@@ -24,32 +24,29 @@ fn main() {
     let mesh = dex::mesh::tiny_mesh_2d();
 
     // TODO: pick dt based on minimum edge length
+    // (this requires an iterator with access to primal volumes)
     let dt = 0.17;
-    let wave_speed = 1.0;
+    let wave_speed_sq = 1.0f64.powi(2);
 
     // TODO: methods to easily create a nonzero initial state
     let mut state = State {
         p: mesh.new_zero_cochain(),
-        q: mesh.new_zero_cochain(),
+        v: mesh.new_zero_cochain(),
     };
 
-    // TODO: implement star correctly
-    //
-    // TODO: allow multiplying operators by scalars
-    //
-    // TODO: boundary conditions:
-    // operators need to be able to have rows removed
-    // and replaced based on which simplices they correspond to.
-    // this requires some kind of tagging system and APIs for modifying operators
     let ops = Ops {
-        p_step: Box::new(mesh.star() * mesh.d()),
-        q_step: Box::new(mesh.star() * mesh.d()),
+        p_step: Box::new(
+            (dt * wave_speed_sq * mesh.star() * mesh.d() * mesh.star())
+                // Dirichlet boundary implemented by removing rows from the operator
+                .exclude_subset(mesh.boundary()),
+        ),
+        v_step: Box::new(mesh.d()),
     };
 
     let step_count = 60;
     for _ in 0..step_count {
-        state.p += &ops.p_step * &state.q;
-        state.q += &ops.q_step * &state.p;
+        state.p += &ops.p_step * &state.v;
+        state.v += &ops.v_step * &state.p;
     }
 
     // TODO: visualize this somehow
