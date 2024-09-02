4b49d1e649fde8ec77ab23902dba0d73c69c9e08
Mikael Myyrä <mikael.myyrae@gmail.com>
2024-09-02 16:55:33 +0300

    feat!: Access more information through simplex views
    Boundaries, volumes, etc.
    This is technically a breaking change
    because it changes the type signature of the simplex views,
    but doesn't change actual existing API usage

--- crates/dexterior-core/src/mesh.rs
+++ crates/dexterior-core/src/mesh.rs
@@ -157,13 +157,21 @@ impl<const MESH_DIM: usize> SimplicialMesh<MESH_DIM> {
     pub fn get_simplex_by_index<const DIM: usize>(
         &self,
         idx: usize,
-    ) -> SimplexView<'_, DIM, MESH_DIM> {
-        let start_idx = idx * self.simplices[DIM].simplex_size;
-        let idx_range = start_idx..start_idx + self.simplices[DIM].simplex_size;
+    ) -> SimplexView<'_, na::Const<DIM>, MESH_DIM> {
+        self.get_simplex_by_index_impl::<na::Const<DIM>>(idx)
+    }
+
+    fn get_simplex_by_index_impl<Dim: na::DimName>(
+        &self,
+        idx: usize,
+    ) -> SimplexView<'_, Dim, MESH_DIM> {
+        let start_idx = idx * self.simplices[Dim::USIZE].simplex_size;
+        let idx_range = start_idx..start_idx + self.simplices[Dim::USIZE].simplex_size;
         SimplexView {
+            mesh: self,
             index: idx,
-            vertices: &self.vertices,
-            indices: &self.simplices[DIM].indices[idx_range],
+            indices: &self.simplices[Dim::USIZE].indices[idx_range],
+            _marker: std::marker::PhantomData,
         }
     }
 
@@ -425,7 +433,7 @@ impl<const MESH_DIM: usize> SimplicialMesh<MESH_DIM> {
     pub fn create_subset_from_predicate<const DIM: usize>(
         &mut self,
         name: impl Into<String>,
-        pred: impl Fn(SimplexView<DIM, MESH_DIM>) -> bool,
+        pred: impl Fn(SimplexView<na::Const<DIM>, MESH_DIM>) -> bool,
     ) {
         let bits: fb::FixedBitSet = self
             .simplices::<DIM>()
@@ -589,30 +597,27 @@ impl MeshPrimality for Dual {
 }
 
 //
-// iterators and views
+// views and iterators
 //
 
 /// A view into a single simplex's data.
-///
-/// The data available via this interface is currently quite limited.
-/// Eventually it will be possible to navigate boundaries,
-/// volumes, etc. via these views and [`SimplexIter`],
-/// but these will be added as they become needed in practical applications.
 #[derive(Clone, Copy, Debug)]
-pub struct SimplexView<'a, const DIM: usize, const MESH_DIM: usize> {
+pub struct SimplexView<'a, SimplexDim, const MESH_DIM: usize> {
+    mesh: &'a SimplicialMesh<MESH_DIM>,
     // index of the simplex in the array of simplices
     index: usize,
     indices: &'a [usize],
-    // view into all vertices of the mesh,
-    // indexed into by values in the `indices` slice
-    vertices: &'a [na::SVector<f64, MESH_DIM>],
+    _marker: std::marker::PhantomData<SimplexDim>,
 }
 
-impl<'a, const DIM: usize, const MESH_DIM: usize> SimplexView<'a, DIM, MESH_DIM> {
+impl<'a, SimplexDim, const MESH_DIM: usize> SimplexView<'a, SimplexDim, MESH_DIM>
+where
+    SimplexDim: na::DimName,
+{
     /// Iterate over the vertices of this simplex.
     #[inline]
     pub fn vertices(&self) -> impl '_ + Iterator<Item = na::SVector<f64, MESH_DIM>> {
-        self.indices.iter().map(|i| self.vertices[*i])
+        self.indices.iter().map(|i| self.mesh.vertices[*i])
     }
 
     /// Iterate over the vertex indices of this simplex.
@@ -626,6 +631,98 @@ impl<'a, const DIM: usize, const MESH_DIM: usize> SimplexView<'a, DIM, MESH_DIM>
     pub fn index(&self) -> usize {
         self.index
     }
+
+    /// Get the unsigned volume of this simplex.
+    #[inline]
+    pub fn volume(&self) -> f64 {
+        self.mesh.simplices[SimplexDim::USIZE].volumes[self.index]
+    }
+
+    /// Get the unsigned volume of the dual cell corresponding to this simplex.
+    #[inline]
+    pub fn dual_volume(&self) -> f64 {
+        self.mesh.simplices[SimplexDim::USIZE].dual_volumes[self.index]
+    }
+
+    /// Get the circumcenter of this simplex.
+    #[inline]
+    pub fn circumcenter(&self) -> na::SVector<f64, MESH_DIM> {
+        self.mesh.simplices[SimplexDim::USIZE].circumcenters[self.index]
+    }
+
+    /// Get the barycenter of this simplex.
+    #[inline]
+    pub fn barycenter(&self) -> na::SVector<f64, MESH_DIM> {
+        self.mesh.simplices[SimplexDim::USIZE].barycenters[self.index]
+    }
+}
+
+impl<'a, SimplexDim, const MESH_DIM: usize> SimplexView<'a, SimplexDim, MESH_DIM>
+where
+    SimplexDim: na::DimNameAdd<na::U1>,
+    na::Const<MESH_DIM>: na::DimNameSub<na::DimNameSum<SimplexDim, na::U1>>,
+{
+    /// Iterate over the `k+1`-dimensional simplices on whose boundary this simplex lies.
+    pub fn coboundary(&self) -> BoundaryIter<'_, na::DimNameSum<SimplexDim, na::U1>, MESH_DIM> {
+        BoundaryIter {
+            mesh: self.mesh,
+            index: 0,
+            map_row: self.mesh.simplices[SimplexDim::USIZE]
+                .coboundary_map
+                .row(self.index),
+            _marker: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<'a, SimplexDim, const MESH_DIM: usize> SimplexView<'a, SimplexDim, MESH_DIM>
+where
+    SimplexDim: na::DimNameSub<na::U1>,
+{
+    /// Iterate over the `k-1`-dimensional simplices on the boundary of this simplex.
+    pub fn boundary(&self) -> BoundaryIter<'_, na::DimNameDiff<SimplexDim, na::U1>, MESH_DIM> {
+        BoundaryIter {
+            mesh: self.mesh,
+            index: 0,
+            map_row: self.mesh.simplices[SimplexDim::USIZE]
+                .boundary_map
+                .row(self.index),
+            _marker: std::marker::PhantomData,
+        }
+    }
+}
+
+/// An iterator over the boundary or coboundary of a simplex,
+/// obtained with [`SimplexView::coboundary`] or [`SimplexView::boundary`].
+///
+/// The iterator returns a pair `(orientation, simplex)`
+/// where orientation is the relative orientation
+/// between the boundary and coboundary simplices in question.
+pub struct BoundaryIter<'a, SimplexDim, const MESH_DIM: usize> {
+    mesh: &'a SimplicialMesh<MESH_DIM>,
+    index: usize,
+    map_row: nas::csr::CsrRow<'a, i8>,
+    _marker: std::marker::PhantomData<SimplexDim>,
+}
+
+impl<'a, SimplexDim, const MESH_DIM: usize> Iterator for BoundaryIter<'a, SimplexDim, MESH_DIM>
+where
+    SimplexDim: na::DimName,
+{
+    type Item = (i8, SimplexView<'a, SimplexDim, MESH_DIM>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let cols = self.map_row.col_indices();
+        if self.index >= cols.len() {
+            return None;
+        }
+        let next_idx = cols[self.index];
+        let next_ori = self.map_row.values()[self.index];
+
+        self.index += 1;
+
+        Some((next_ori, self.mesh.get_simplex_by_index_impl(next_idx)))
+    }
 }
 
 /// Iterator over a set of `DIM`-simplices in a mesh.
@@ -637,7 +734,7 @@ pub struct SimplexIter<'a, const DIM: usize, const MESH_DIM: usize> {
 }
 
 impl<'a, const DIM: usize, const MESH_DIM: usize> Iterator for SimplexIter<'a, DIM, MESH_DIM> {
-    type Item = SimplexView<'a, DIM, MESH_DIM>;
+    type Item = SimplexView<'a, na::Const<DIM>, MESH_DIM>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.index >= self.len {
@@ -665,5 +762,90 @@ mod tests {
         let pred_subset = mesh.get_subset::<1>("predicate").unwrap();
         assert_eq!(idx_subset.indices, pred_subset.indices);
         itertools::assert_equal(idx_subset.indices.ones(), indices.iter().cloned());
+    }
+
+    #[test]
+    fn iterators() {
+        let mesh = tiny_mesh_2d();
+        // for a quick sanity test, check that every edge
+        // is on the coboundary of all its boundary simplices and vice versa
+        for edge in mesh.simplices::<1>() {
+            for (b_ori, b) in edge.boundary() {
+                let (c_ori, _c) = b
+                    .coboundary()
+                    .find(|(_, e)| e.index() == edge.index())
+                    .expect("edge must be on its boundary's coboundary");
+                assert_eq!(
+                    b_ori, c_ori,
+                    "boundary and coboundary orientations should agree"
+                );
+            }
+
+            for (b_ori, b) in edge.coboundary() {
+                let (c_ori, _c) = b
+                    .boundary()
+                    .find(|(_, e)| e.index() == edge.index())
+                    .expect("edge must be on its coboundary's boundary");
+                assert_eq!(
+                    b_ori, c_ori,
+                    "boundary and coboundary orientations should agree"
+                );
+            }
+        }
+
+        // ensure simplex views are accessing the correct data
+
+        // volume
+        itertools::assert_equal(
+            mesh.simplices::<0>().map(|s| s.volume()),
+            mesh.simplices[0].volumes.iter().cloned(),
+        );
+        itertools::assert_equal(
+            mesh.simplices::<1>().map(|s| s.volume()),
+            mesh.simplices[1].volumes.iter().cloned(),
+        );
+        itertools::assert_equal(
+            mesh.simplices::<2>().map(|s| s.volume()),
+            mesh.simplices[2].volumes.iter().cloned(),
+        );
+        // dual volume
+        itertools::assert_equal(
+            mesh.simplices::<0>().map(|s| s.dual_volume()),
+            mesh.simplices[0].dual_volumes.iter().cloned(),
+        );
+        itertools::assert_equal(
+            mesh.simplices::<1>().map(|s| s.dual_volume()),
+            mesh.simplices[1].dual_volumes.iter().cloned(),
+        );
+        itertools::assert_equal(
+            mesh.simplices::<2>().map(|s| s.dual_volume()),
+            mesh.simplices[2].dual_volumes.iter().cloned(),
+        );
+        // circumcenter
+        itertools::assert_equal(
+            mesh.simplices::<0>().map(|s| s.circumcenter()),
+            mesh.simplices[0].circumcenters.iter().cloned(),
+        );
+        itertools::assert_equal(
+            mesh.simplices::<1>().map(|s| s.circumcenter()),
+            mesh.simplices[1].circumcenters.iter().cloned(),
+        );
+        itertools::assert_equal(
+            mesh.simplices::<2>().map(|s| s.circumcenter()),
+            mesh.simplices[2].circumcenters.iter().cloned(),
+        );
+        // barycenter
+        itertools::assert_equal(
+            mesh.simplices::<0>().map(|s| s.barycenter()),
+            mesh.simplices[0].barycenters.iter().cloned(),
+        );
+        itertools::assert_equal(
+            mesh.simplices::<1>().map(|s| s.barycenter()),
+            mesh.simplices[1].barycenters.iter().cloned(),
+        );
+        itertools::assert_equal(
+            mesh.simplices::<2>().map(|s| s.barycenter()),
+            mesh.simplices[2].barycenters.iter().cloned(),
+        );
     }
 }
