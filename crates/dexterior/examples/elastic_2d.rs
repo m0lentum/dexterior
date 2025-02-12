f927a6f96e8eba9bd751f97596c250be79625e52
Mikael Myyrä <mikael.myyrae@gmail.com>
2025-02-12 15:30:26 +0200

    WIP: Break the elastic wave example

--- crates/dexterior/examples/elastic_2d.rs
+++ crates/dexterior/examples/elastic_2d.rs
@@ -4,15 +4,15 @@ use dexterior as dex;
 use dexterior_visuals as dv;
 use nalgebra as na;
 
-type Pressure = dex::Cochain<0, dex::Primal>;
-type Velocity = dex::Cochain<1, dex::Primal>;
-type Shear = dex::Cochain<0, dex::Dual>;
+type Pressure = dex::Cochain<0, dex::Dual>;
+type Flux = dex::Cochain<1, dex::Primal>;
+type Shear = dex::Cochain<2, dex::Dual>;
 
 #[derive(Clone, Debug)]
 struct State {
     t: f64,
     p: Pressure,
-    v: Velocity,
+    q: Flux,
     w: Shear,
 }
 
@@ -21,17 +21,17 @@ impl dv::AnimationState for State {
         Self {
             t: old.t + t * (new.t - old.t),
             p: old.p.lerp(&new.p, t),
-            v: old.v.lerp(&new.v, t),
+            q: old.q.lerp(&new.q, t),
             w: old.w.lerp(&new.w, t),
         }
     }
 }
 
 struct Ops {
-    p_step: dex::Op<Velocity, Pressure>,
-    v_step_p: dex::Op<Pressure, Velocity>,
-    v_step_w: dex::Op<Shear, Velocity>,
-    w_step: dex::Op<Velocity, Shear>,
+    p_step: dex::Op<Flux, Pressure>,
+    q_step_p: dex::Op<Pressure, Flux>,
+    q_step_w: dex::Op<Shear, Flux>,
+    w_step: dex::Op<Flux, Shear>,
 }
 
 struct MaterialArea {
@@ -77,6 +77,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let bottom_edges = mesh.get_subset::<1>("990").expect("Subset not found");
     let bottom_verts = mesh.get_subset::<0>("990").expect("Subset not found");
+    let bottom_adjacent_dual_verts =
+        dex::Subset::from_cell_iter(mesh.simplices_in(&bottom_edges).map(|edge| {
+            let (_orientation, adjacent_tri) = edge.boundary().next().unwrap();
+            adjacent_tri.dual()
+        }));
 
     // other parameters
 
@@ -89,8 +94,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // operators
 
     // spatially varying scaling factors
-    let stiffness_scaling = mesh.scaling(|s| {
-        let l = layers.iter().find(|l| l.verts.contains(s)).unwrap();
+    let stiffness_scaling = mesh.scaling_dual(|s| {
+        let l = layers.iter().find(|l| l.tris.contains(s.dual())).unwrap();
         l.stiffness
     });
     let density_scaling = mesh.scaling(|s| {
@@ -98,15 +103,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         1. / l.density
     });
     let mu_scaling = mesh.scaling_dual(|s| {
-        let l = layers.iter().find(|l| l.tris.contains(s.dual())).unwrap();
+        let l = layers.iter().find(|l| l.verts.contains(s.dual())).unwrap();
         l.mu
     });
 
     let ops = Ops {
-        p_step: -dt * stiffness_scaling * mesh.star() * mesh.d() * mesh.star(),
-        v_step_p: dt * density_scaling.clone() * mesh.d(),
-        v_step_w: dt * density_scaling * mesh.star() * mesh.d(),
-        w_step: dt * mu_scaling * mesh.star() * mesh.d(),
+        p_step: -dt * stiffness_scaling * mesh.star() * mesh.d(),
+        q_step_p: dt * density_scaling.clone() * mesh.star() * mesh.d(),
+        q_step_w: -dt * density_scaling * mesh.d() * mesh.star(),
+        w_step: dt * mu_scaling * mesh.d() * mesh.star(),
     };
 
     // source terms
@@ -123,9 +128,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         if t > shear_angular_vel * PI {
             return 0.;
         }
-        let normal = na::Vector2::new(dir.y, -dir.x);
         let vel = -shear_wave_vector * f64::sin(shear_angular_vel * t - shear_wave_vector.dot(&p));
-        vel.dot(&normal)
+        vel.dot(&dir)
     };
 
     // run simulation
@@ -133,7 +137,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let state = State {
         t: 0.,
         p: mesh.new_zero_cochain(),
-        v: mesh.new_zero_cochain(),
+        q: mesh.new_zero_cochain(),
         w: mesh.new_zero_cochain(),
     };
 
@@ -151,17 +155,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             // and should probably be provided in the parameters to the step function here,
             // instead of users having to include it in their state
             state.t += dt;
-            state.v += &ops.v_step_p * &state.p + &ops.v_step_w * &state.w;
-            state.p += &ops.p_step * &state.v;
-            state.w += &ops.w_step * &state.v;
+            state.q += &ops.q_step_p * &state.p + &ops.q_step_w * &state.w;
+            state.p += &ops.p_step * &state.q;
+            state.w += &ops.w_step * &state.q;
             // sources on the bottom edge
+            // mesh.integrate_overwrite(
+            //     &mut state.p,
+            //     bottom_verts,
+            //     dex::quadrature::Pointwise(|p| pressure_source(p, state.t)),
+            // );
             mesh.integrate_overwrite(
-                &mut state.p,
-                &bottom_verts,
-                dex::quadrature::Pointwise(|p| pressure_source(p, state.t)),
-            );
-            mesh.integrate_overwrite(
-                &mut state.v,
+                &mut state.q,
                 &bottom_edges,
                 dex::quadrature::GaussLegendre6(|p, d| shear_source(p, d, state.t)),
             );
@@ -169,10 +173,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         draw: |state, draw| {
             draw.axes_2d(dv::AxesParams::default());
             // draw.vertex_colors(&state.p);
-            draw.triangle_colors_dual(&state.w);
+            draw.triangle_colors_dual(&state.p);
             draw.wireframe(dv::WireframeParams::default());
-            draw.velocity_arrows(
-                &state.v,
+            draw.flux_arrows(
+                &state.q,
                 dv::ArrowParams {
                     scaling: 0.3,
                     ..Default::default()
